f2d1e24d4799c6cdfde1406eff94d85684809da1
lyh20161104592 <lyh20161104592@Mac-20.local>
2017-06-12 11:58:02 +0800

    qwe
    awe

--- 排序/main.c
+++ 排序/main.c
@@ -5,11 +5,33 @@
 //  Created by lyh20161104592 on 17/6/12.
 //  Copyright © 2017年 lyh20161104592. All rights reserved.
 //
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main()
+{
+    int a[10];
+    int j,i,k,n;
+    int temp;
+    printf("请输入十个整数\n");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=0;j<9;j++)
+    {
+        for(k=0;k<9-j;k++)
+        {
+          if(a[i]>a[i+1])
+            {
+            temp=a[i+1];
+            a[i]=temp;
+            a[i+1]=a[i];
+             }
+        }
+        for(k=0;k<i;k++)
+        {
+            printf("%d",a[i]);
+        }
+    }
+        printf("\n");
+        return 0;
 }
